fd0e92bfe67fddb64c2fe094d8551e26780c956c
Yasser Fadl <yasser.fadl@gmail.com>
2019-07-31 12:58:52 +0300

    fixing tslint issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { TOPIC, JSONObject, STATE_REGISTRY_TOPIC } from '@deepstream/protobuf/dist/types/all'
 import { Message, BulkSubscriptionMessage, SubscriptionMessage, ALL_ACTIONS } from '@deepstream/protobuf/dist/types/messages'
 
-export declare type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+export declare type Primitive = string | number | boolean | bigint | symbol | undefined | null
 type DeepPartial<T> = T extends Primitive ? T : T extends Function ? T : T extends Date ? T : T extends Map<infer K, infer V> ? DeepPartialMap<K, V> : T extends Set<infer U> ? DeepPartialSet<U> : T extends {} ? {
-  [K in keyof T]?: DeepPartial<T[K]>;
-} : Partial<T>;
+  [KK in keyof T]?: DeepPartial<T[KK]>;
+} : Partial<T>
 interface DeepPartialSet<ItemType> extends Set<DeepPartial<ItemType>> {
 }
 interface DeepPartialMap<KeyType, ValueType> extends Map<DeepPartial<KeyType>, DeepPartial<ValueType>> {
