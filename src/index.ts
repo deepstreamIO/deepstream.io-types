4ace5b36d673fbdbf9643b496eb2015a894b4c0e
jaime-ez <jechegarayz@gmail.com>
2020-04-28 00:01:52 -0400

    [fix] final interface
    :100644 100644 0c6839e 5c3861e M	src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,12 +30,15 @@ export abstract class Handler<SpecificMessage> {
   public async close (): Promise<void> {}
 }
 
-export interface SimpleSocketWrapper {
+export interface SocketData {
   socketType: string
   userId: string | null
   clientData: object | null
-  serverData: object | null
+  serverData: object | null,
   isRemote?: boolean
+}
+
+export interface SimpleSocketWrapper extends SocketData {
   parseMessage (serializedMessage: any): ParseResult[]
   sendMessage (message: Message, buffer?: boolean): void
   sendAckMessage (message: Message, buffer?: boolean): void
@@ -237,15 +240,10 @@ export interface DeepstreamCache extends DeepstreamStorage  {
   headBulk (recordNames: string[], callback: StorageHeadBulkCallback): void
 }
 
-export interface SocketUserData {
-  userId: string,
-  serverData: JSONObject | null
-}
-
 export interface DeepstreamMonitoring extends DeepstreamPlugin  {
   onErrorLog (loglevel: LOG_LEVEL, event: EVENT, logMessage: string, metaData: MetaData): void
   onLogin (allowed: boolean, endpointType: string): void
-  onMessageReceived (message: Message, socketUserData: SocketUserData): void
+  onMessageReceived (message: Message, socketData: SocketData): void
   onMessageSend (message: Message): void
   onBroadcast (message: Message, count: number): void
 }
